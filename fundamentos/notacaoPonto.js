9ba2ce3e49316c1b9f910104e0a250ea345df14e
mateus <mateusbritodecarvalho19@gmail.com>
2023-07-19 19:43:18 -0300

    fundamentos

---
+++ fundamentos/notacaoPonto.js
@@ -0,0 +1,40 @@
+console.log(Math.ceil(6.1))
+
+const obj1 = {}
+obj1.nome = 'Bola'
+// obj1['nome'] = 'Bola2'
+console.log(obj1.nome)
+
+function Obj(nome){
+    this.nome = nome
+    this.exec = function() {
+        console.log('Exec...')
+    }
+}
+
+const obj2 = new Obj('Cadeira')
+const obj3 = new Obj('Mesa')
+const obj = new Obj('marca')
+
+console.log(obj2.nome)
+console.log(obj3.nome)
+obj.exec()
+
+class Coisa {
+    constructor(nome) { 
+        this.nome = nome
+    }
+
+    exec() {
+        console.log('Execultando..');
+        console.log('Meu nome é', this.nome);
+    }
+}
+
+const coisa1 = new Coisa('Celular');
+
+coisa1.exec();
+
+coisa1.nome = 'Teclado';
+
+coisa1.exec();
---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,12 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer =  3.151692
+console.log(qualquer)
+console.log(typeof qualquer)
+
+//Ao utilizar uma variável, evitar nome genéricos e siglas.
+let valor = ''
+let numero = 1
+let pqp = false // Produto Químico Perigoso... kkk!
